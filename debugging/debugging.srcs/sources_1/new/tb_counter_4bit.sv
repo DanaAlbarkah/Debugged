9daa7b8ba2084ee6657a3f4e8c3dfa54b425e34d
dana <danah.albarkah@gmail.com>
2024-12-10 11:43:47 +0300

    Exam

---
+++ debugging/debugging.srcs/sources_1/new/tb_counter_4bit.sv
@@ -0,0 +1,41 @@
+`timescale 1ns / 1ps
+
+module tb_counter_4bit;
+logic clk;           
+logic reset_n;       
+logic load;       
+logic [3:0] load_data;
+ logic [3:0] count;   
+
+ counter_4bit DUT(
+    .*
+);
+      always #5 clk=~clk;
+
+    initial begin
+       
+        clk = 0;
+        reset_n=0;
+        load=0;
+        
+#10;
+load=1;
+reset_n=1;
+load=1;
+load_data=1;  
+  #10;
+load=0;
+reset_n=1;
+load=1;
+load_data=1;
+ #10;
+load=0;
+reset_n=1;
+load=1;
+load_data=1;  
+        #50;
+      
+        
+        $stop;
+    end
+endmodule
